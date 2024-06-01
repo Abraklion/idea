f3b1966c65352457a381b5c5659167816d9fb10f
Abraklion <abraklion@gmail.com>
2024-06-01 11:08:00 +0500

    Структура HTML

---
+++ dist/js/main.js
@@ -0,0 +1,20 @@
+(self["webpackChunkgulp_html_scss"] = self["webpackChunkgulp_html_scss"] || []).push([["main"],{
+
+/***/ "./src/js/main.ts":
+/*!************************!*\
+  !*** ./src/js/main.ts ***!
+  \************************/
+/***/ (function() {
+
+console.log('ok');
+
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/js/main.ts"));
+/******/ }
+]);
+//# sourceMappingURL=main.js.map
